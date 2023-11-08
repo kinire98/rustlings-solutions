aec1d7c52aa3bcea3031edfe22355687123a50c1
kinire98 <iknitgarri@outlook.com>
2023-11-08 12:46:24 +0100

    ARustlings solutions

---
+++ clippy/target/debug/deps/clippy3-a5eff4bc49f1b6c8.d
@@ -0,0 +1,8 @@
+C:\Users\Iknit\Documents\Proyectos\rustlings\exercises\clippy\target\debug\deps\clippy3-a5eff4bc49f1b6c8.rmeta: clippy3.rs Cargo.toml
+
+C:\Users\Iknit\Documents\Proyectos\rustlings\exercises\clippy\target\debug\deps\clippy3-a5eff4bc49f1b6c8.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
